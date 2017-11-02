2e5e2c6433dc4b934dfd2f5443a5b7f31e5bbc39
erva1600 <erva1600@COSQ260PC10>
2017-11-02 11:51:32 +0100

    1

---
+++ Up3/Up3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Up3.MvcApplication" Language="C#" %>
---
+++ Up3/Up3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Up3.Startup))]
+namespace Up3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
